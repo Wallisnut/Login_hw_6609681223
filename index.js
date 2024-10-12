ae8a73c86e69b420a2975adbc541957f14b63a58
newbie <thanagon.pay@dome.tu.ac.th>
2024-10-12 14:58:23 +0700

    htmlconfig

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
+  res.sendFile(path.join(__dirname, "public", "HTML", "login_p.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
