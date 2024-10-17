18ff560398bfb1af3eb47499f9cacbe995985ec5
newbie <thanagon.pay@dome.tu.ac.th>
2024-10-17 11:18:51 +0700

    config

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,75 +1,77 @@
 function submitLogin(event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const role = document.getElementById('select-btn').value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const role = document.getElementById("select-btn").value;
 
   if (!username || !password) {
     alert("Please fill in both fields");
     return;
   }
-  if(role=="--select your role--"){
+  if (role == "--select your role--") {
     alert("Please select role");
+    return;
   }
 
   const loginData = {
     UserName: username,
-    PassWord: password
+    PassWord: password,
   };
 
-  fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Application-Key': 'TUa7110a94484554d8a529ad3eb1438f4d1b07d08b86946c3b8a07faa1c56efdc7061e2be45ca8c1d6380e08f1df7d135d'
-      },
-      body: JSON.stringify(loginData)
+  fetch("https://restapi.tu.ac.th/api/v1/auth/Ad/verify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Application-Key":
+        "TUa7110a94484554d8a529ad3eb1438f4d1b07d08b86946c3b8a07faa1c56efdc7061e2be45ca8c1d6380e08f1df7d135d",
+    },
+    body: JSON.stringify(loginData),
   })
-  .then(response => {
+    .then((response) => {
       if (!response.ok) {
-          throw new Error('Network response was not ok');
+        throw new Error("Incorrect Credentials");
       }
       return response.json();
-  })
-  .then(data => {
-    const resultElement = document.getElementById('b');
-    const resultText = `Username: ${data.displayname_th}
+    })
+    .then((data) => {
+      const resultElement = document.getElementById("b");
+      const resultText = `Username: ${data.displayname_th}
+    Student ID: ${data.username}
     Department: ${data.department}
     Faculty: ${data.faculty}`;
-    resultElement.innerText = resultText;
-    resultElement.style.fontSize = '25px';
-  })
-  .catch(error => console.error('Error:', error));
+      resultElement.innerText = resultText;
+      resultElement.style.fontSize = "25px";
+    })
+    .catch((error) => console.error("Error:", error));
 }
 function call_REST_API_Hello() {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
   if (!username || !password) {
-      alert("Please fill in both fields");
-      return;
+    alert("Please fill in both fields");
+    return;
   }
 
-  const url = (
-      'http://localhost:8000/?' +
-      new URLSearchParams({ myName: username, lastName: password }).toString()
-  );
+  const url =
+    "http://localhost:8000/?" +
+    new URLSearchParams({ myName: username, lastName: password }).toString();
 
   fetch(url)
-  .then(response => {
+    .then((response) => {
       if (!response.ok) {
-          throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.text();
-  })
-  .then(text => {
-    const resultElement = document.getElementById('b');
-    const resultText = `Username: ${text.displayname_th}
+    })
+    .then((text) => {
+      const resultElement = document.getElementById("b");
+      const resultText = `Username: ${text.displayname_th}
     Department: ${text.department}
     Faculty: ${text.faculty}`;
-    resultElement.innerText = resultText;
-    resultElement.style.fontSize = '25px';
-  })
-  .catch(error => console.error('Error:', error));
+      resultElement.innerText = resultText;
+      resultElement.style.fontSize = "25px";
+    })
+    .catch((error) => console.error("Error:", error));
 }
